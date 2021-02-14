c95b7751e8f8cf92545c21b96fde81ef651c5bed
Bestami Tayfuroğlu <bestami.tayfuroglu@hotmail.com>
2021-02-14 17:08:03 +0300

    Add project files.

---
+++ GameProject/UserValidationManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+	class UserValidationManager : IUserValidationService
+	{
+		public bool Validation(Gamer gamer)
+		{
+			if (gamer.BirthYear == 1986 && gamer.FirstName == "Bestami" && gamer.LastName == "Tayfuroğlu" && gamer.IdentityNumber == 123456)
+			{
+
+				return true;
+			}
+			else
+			{
+
+				return false;
+			}
+		}
+	}
+}
